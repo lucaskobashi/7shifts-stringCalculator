5386390dd8c2e3674d930017661ede46bf2e86df
lucas kobashi <lucas.kobashi@cs.usask.ca>
2022-02-03 22:22:46 -0600

    created stub function

---
+++ stringCalculator.py
@@ -0,0 +1,12 @@
+# Lucas Kobashi dos Anjos
+# lucas.kobashi@cs.usask.ca
+
+def Add(numbers):
+    '''
+
+    :param numbers:
+    :return:
+    '''
+    if numbers == 0:
+        return 0
+    else:
