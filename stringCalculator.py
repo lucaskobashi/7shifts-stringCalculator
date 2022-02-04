86d1f7b40bcff0f82a34050a94152b4f9b6c7eb3
lucas kobashi <lucas.kobashi@cs.usask.ca>
2022-02-03 22:29:15 -0600

    finished part 1

--- stringCalculator.py
+++ stringCalculator.py
@@ -3,10 +3,32 @@
 
 def Add(numbers):
     '''
-
-    :param numbers:
-    :return:
+    Adds all numbers
+    :param numbers: String, numbers separated by a comma
+    :return: Int, sum of numbers
     '''
-    if numbers == 0:
+    if numbers == "":
         return 0
-    else:
+    numList = numbers.strip().split(',')
+    total = 0
+    for n in numList:
+        total += int(n)
+
+    return total
+
+# test drivers
+
+if Add("") != 0:
+    print("Error! test with empty string")
+
+if Add("1") != 1:
+    print("Error! test with single item")
+
+if Add("1,2,5") != 8:
+    print("Error! test with items separated without space")
+
+if Add("1, 3, 5") != 9:
+    print("Error! test with items separated with single space")
+
+if Add("2,  4,  6") != 12:
+    print("Error! test with items separated with multiple spaces")
